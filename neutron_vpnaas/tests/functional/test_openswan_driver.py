7bb70fa4d1b351e1e5b13bc639c554d503e93fa5
Paul Michali <pc@michali.net>
2015-01-28 16:41:57 -0500

    Provide hooks for VPNaaS repo functional gate
    This provides the needed hook files so that functional tests
    can be run in the neutron-vpnaas repo.
    
    A placeholder test case is added, to actually exercise the
    gate. In a separate commit, real test cases will be added
    for testing the OpenSwan implementation.  Just adding the
    placeholder here, to get around the chicken and egg problem
    of having a test to check the gate.
    
    Change-Id: I1b48ce261181ec066c9951bb81a0876a9862311a
    Partial-Bug: 1412770

---
+++ neutron_vpnaas/tests/functional/test_openswan_driver.py
@@ -0,0 +1,66 @@
+# Copyright (c) 2015 Cisco Systems, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+# NOTE: The purpose of this module is to provide a nop test to verify that
+# the functional gate is working.
+
+# TODO(pcm): In the future, repurpose this module for use in a "real"
+# functional test for the OpenSwan reference implementation. For now, just
+# ignore the test cases herein.
+
+from neutron.tests.functional.agent.linux import base
+
+
+class TestOpenSwanDeviceDriver(base.BaseLinuxTestCase):
+
+    """Test the OpenSwan reference implmentation of the device driver."""
+
+    # NOTE: Tests may be added/removed/changed, when this is fleshed out
+    # in future commits.
+
+    def test_config_files_created_on_ipsec_connection_create(self):
+        """Verify that directory and config files are correct on create."""
+        pass
+
+    def test_config_files_removed_on_ipsec_connection_delete(self):
+        """Verify that directory and config files removed on delete."""
+        pass
+
+    def test_process_created_on_ipsec_connection_create(self):
+        """Check that pluto process is running."""
+        pass
+
+    def test_connection_status_with_one_side_of_ipsec_connection(self):
+        """Check status of connection, with only one end created.
+
+        Expect that the status will indicate that the connection is down.
+        """
+        pass
+
+    def test_process_gone_on_ipsec_connection_delete(self):
+        """Verify that there is no longer a process, upon deletion."""
+        pass
+
+    def test_nat_rule_update(self):
+        """Check NAT rule when create and then delete connection."""
+        pass
+
+    def test_cached_status_on_create_and_delete(self):
+        """Test that the status is cached."""
+        pass
+
+    def test_status_reporting(self):
+        """Test status reported correctly to agent."""
+        pass
