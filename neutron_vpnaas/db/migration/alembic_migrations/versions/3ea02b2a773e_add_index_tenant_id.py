e055639ec9b152fbdec211364e01ed865e1c6032
Ann Kamyshnikova <akamyshnikova@mirantis.com>
2015-02-10 17:59:16 +0300

    Add index on tenant_id
    Adding index on tenant_id could greatly speed up SELECTs with
    filters by tenant_id when some value is selected per resource
    per tenant.
    As a continuation part of https://review.openstack.org/143120 for
    service tables.
    
    Change-Id: I9f67a82b0e0a493ff8b39424296699e7e517e9df
    Partial-bug: #1419815

---
+++ neutron_vpnaas/db/migration/alembic_migrations/versions/3ea02b2a773e_add_index_tenant_id.py
@@ -0,0 +1,43 @@
+# Copyright 2015 OpenStack Foundation
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+"""add_index_tenant_id
+
+Revision ID: 3ea02b2a773e
+Revises: start_neutron_vpnaas
+Create Date: 2015-02-10 17:51:10.752504
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '3ea02b2a773e'
+down_revision = 'start_neutron_vpnaas'
+
+from alembic import op
+
+TABLES = ['ipsecpolicies', 'ikepolicies', 'ipsec_site_connections',
+          'vpnservices']
+
+
+def upgrade():
+    for table in TABLES:
+        op.create_index(op.f('ix_%s_tenant_id' % table),
+                        table, ['tenant_id'], unique=False)
+
+
+def downgrade():
+    for table in TABLES:
+        op.drop_index(op.f('ix_%s_tenant_id' % table),
+                      table_name=table)
