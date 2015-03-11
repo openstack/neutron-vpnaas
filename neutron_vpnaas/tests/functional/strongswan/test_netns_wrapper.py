cbc32a79d29a844d58bf5b3c5d2a77d1448aa639
Zhang Hua <joshua.zhang@canonical.com>
2015-03-11 10:08:18 +0800

    Functional tests of ipsec strongswan vpnaas driver
    This is functional test patch for strongswan driver commit [1].
    
    [1], https://review.openstack.org/#/c/144391/
    
    Closes-Bug: 1433223
    
    Change-Id: I02ae7bb32fd902c24c46c090bc863b392eafe259

---
+++ neutron_vpnaas/tests/functional/strongswan/test_netns_wrapper.py
@@ -0,0 +1,55 @@
+# Copyright (c) 2015 Canonical, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import re
+
+from neutron.agent.common import config
+from neutron.agent.linux import utils
+from neutron.tests.functional.agent.linux import base
+
+WRAPPER_SCRIPT = 'neutron-vpn-netns-wrapper'
+STATUS_PATTERN = re.compile('Command:.*ip.*addr.*show.*Exit code: 0')
+
+
+class TestNetnsWrapper(base.BaseLinuxTestCase):
+
+    def setUp(self):
+        super(TestNetnsWrapper, self).setUp()
+        config.setup_logging()
+        self.fake_ns = 'func-8f1b728c-6eca-4042-9b6b-6ef66ab9352a'
+        self.mount_paths = ('--mount_paths=/etc:/var/lib/neutron'
+                            '/vpnaas/%(ns)s/etc,/var/run:/var/lib'
+                            '/neutron/vpnaas/%(ns)s/var/run')
+        self.fake_pth = self.mount_paths % {'ns': self.fake_ns}
+
+    def test_netns_wrap_success(self):
+        client_ns = self._create_namespace()
+        ns = client_ns.namespace
+        pth = self.mount_paths % {'ns': ns}
+        cmd = WRAPPER_SCRIPT, pth, '--cmd=ip,addr,show'
+        output = client_ns.netns.execute(cmd)
+        self.assertTrue(STATUS_PATTERN.search(output))
+
+    def test_netns_wrap_fail_without_netns(self):
+        cmd = [WRAPPER_SCRIPT, self.fake_pth,
+               '--cmd=ip,addr,show']
+        self.assertRaises(RuntimeError, utils.execute, cmd=cmd,
+                          run_as_root=True)
+
+    def test_netns_wrap_unauthorized_command(self):
+        cmd = [WRAPPER_SCRIPT, self.fake_pth,
+               '--cmd=nofiltercommand']
+        self.assertRaises(RuntimeError, utils.execute, cmd=cmd,
+                          run_as_root=True)
--- neutron_vpnaas/tests/functional/strongswan/test_strongswan_driver.py
+++ neutron_vpnaas/tests/functional/strongswan/test_strongswan_driver.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2015 Canonical, Inc.
+# All Rights Reserved.
+#
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
 #    a copy of the License at
@@ -10,20 +13,131 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-# NOTE: The purpose of this module is to provide a nop test to verify that
-# the functional gate is working for the StrongSwan device driver.
+import mock
+import os
 
-# TODO(pcm): In the future, this module will be replace, in whole, with
-# actual StrongSwan driver test classes/cases.
-
-
+from neutron.agent.l3 import config as l3_config
+from neutron.agent.l3 import legacy_router
+from neutron.openstack.common import uuidutils
+from neutron.plugins.common import constants
 from neutron.tests.functional.agent.linux import base
+from oslo_config import cfg
+
+from neutron_vpnaas.services.vpn.device_drivers import ipsec
+from neutron_vpnaas.services.vpn.device_drivers import strongswan_ipsec
+
+_uuid = uuidutils.generate_uuid
+FAKE_ROUTER_ID = _uuid()
+FAKE_IPSEC_SITE_CONNECTION1_ID = _uuid()
+FAKE_IPSEC_SITE_CONNECTION2_ID = _uuid()
+FAKE_IKE_POLICY = {
+    'ike_version': 'v1',
+    'encryption_algorithm': 'aes-128',
+    'auth_algorithm': 'sha1',
+    'pfs': 'group5'
+}
+
+FAKE_IPSEC_POLICY = {
+    'encryption_algorithm': 'aes-128',
+    'auth_algorithm': 'sha1',
+    'pfs': 'group5'
+}
+
+FAKE_VPN_SERVICE = {
+    'id': _uuid(),
+    'router_id': FAKE_ROUTER_ID,
+    'name': 'myvpn',
+    'admin_state_up': True,
+    'status': constants.PENDING_CREATE,
+    'external_ip': '50.0.0.4',
+    'subnet': {'cidr': '10.0.0.0/24'},
+    'ipsec_site_connections': [
+        {'peer_cidrs': ['20.0.0.0/24',
+                        '30.0.0.0/24'],
+         'id': FAKE_IPSEC_SITE_CONNECTION1_ID,
+         'peer_address': '30.0.0.5',
+         'peer_id': '30.0.0.5',
+         'psk': 'password',
+         'initiator': 'bi-directional',
+         'ikepolicy': FAKE_IKE_POLICY,
+         'ipsecpolicy': FAKE_IPSEC_POLICY,
+         'status': constants.PENDING_CREATE},
+        {'peer_cidrs': ['40.0.0.0/24',
+                        '50.0.0.0/24'],
+         'peer_address': '50.0.0.5',
+         'peer_id': '50.0.0.5',
+         'psk': 'password',
+         'id': FAKE_IPSEC_SITE_CONNECTION2_ID,
+         'initiator': 'bi-directional',
+         'ikepolicy': FAKE_IKE_POLICY,
+         'ipsecpolicy': FAKE_IPSEC_POLICY,
+         'status': constants.PENDING_CREATE}]
+}
+
+DESIRED_CONN_STATUS = {FAKE_IPSEC_SITE_CONNECTION1_ID:
+                       {'status': 'DOWN',
+                        'updated_pending_status': False},
+                       FAKE_IPSEC_SITE_CONNECTION2_ID:
+                       {'status': 'DOWN',
+                        'updated_pending_status': False}}
 
 
 class TestStrongSwanDeviceDriver(base.BaseLinuxTestCase):
 
-    """Test the StrongSwan reference implmentation of the device driver."""
+    """Test the StrongSwan reference implementation of the device driver."""
 
-    def test_dummy(self):
-        """Placeholder test for verifying the functional job."""
-        pass
+    def setUp(self):
+        super(TestStrongSwanDeviceDriver, self).setUp()
+        self.conf = cfg.CONF
+        self.conf.use_namespaces = True
+        self.conf.register_opts(l3_config.OPTS)
+        self.conf.register_opts(ipsec.ipsec_opts, 'ipsec')
+        self.conf.register_opts(strongswan_ipsec.strongswan_opts,
+                                'strongswan')
+        self.conf.set_override('state_path', '/tmp')
+
+        ri_kwargs = {'router': {'id': FAKE_ROUTER_ID},
+                     'agent_conf': self.conf,
+                     'interface_driver': mock.sentinel.interface_driver}
+        self.router = legacy_router.LegacyRouter(FAKE_ROUTER_ID, **ri_kwargs)
+        self.router.router['distributed'] = False
+        self.router_id = FAKE_VPN_SERVICE['router_id']
+
+        looping_call_p = mock.patch(
+            'neutron.openstack.common.loopingcall.FixedIntervalLoopingCall')
+        looping_call_p.start()
+
+        self.driver = strongswan_ipsec.StrongSwanDriver(
+            agent=mock.Mock(), host=mock.sentinel.host)
+        self.driver.routers[FAKE_ROUTER_ID] = self.router
+        self.driver.agent_rpc = mock.Mock()
+        self.driver._update_nat = mock.Mock()
+        self.driver.agent_rpc.get_vpn_services_on_host.return_value = [
+            FAKE_VPN_SERVICE]
+        self.addCleanup(self.driver.destroy_router, self.router_id)
+
+        self.conf.set_override('router_delete_namespaces', True)
+        self.router.router_namespace.create()
+        self.addCleanup(self.router.router_namespace.delete)
+
+    def test_process_lifecycle(self):
+        """
+        Lifecycle test that validates that the strongswan process could be
+        launched, that a connection could be successfully initiated through
+        it, and then that it could be terminated and clean up after itself.
+        """
+        process = self.driver.ensure_process(self.router_id,
+                                             FAKE_VPN_SERVICE)
+        process.enable()
+        self.assertTrue(process.active)
+        self.assertIn(self.router_id, self.driver.processes)
+        self.assertEqual(DESIRED_CONN_STATUS, process.connection_status)
+        self.assertIsNotNone(process.namespace)
+
+        conf_dir = os.path.join(self.conf.ipsec.config_base_dir,
+                                self.router_id)
+        self.assertTrue(os.path.exists(conf_dir))
+        process.disable()
+        self.assertFalse(process.active)
+        self.assertFalse(process.connection_status)
+        self.assertFalse(os.path.exists(conf_dir))
