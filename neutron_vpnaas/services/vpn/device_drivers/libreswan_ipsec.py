5cc06138332a06334fa1d4fa28e71c59d58f8aca
venkata anil <anil.venkata@enovance.com>
2015-05-03 10:27:12 +0000

    Libreswan driver support in VPNaaS
    VPNaas is not working on Fedora/centos devstack.
    Fedora/centos uses Libreswan(fork of the Openswan IPSEC VPN) for ipsec.
    
    Libreswan needs nssdb to be  initialised before
    'ipsec pluto' command, otherwise pluto daemon will fail to run
    
    (cherry picked from commit 72e1f670fdca2138aa85703cb96eac473f6fb811)
    Change-Id: I54558208b2aaa82bda09c0db96042d236eceba69
    Closes-bug: #1444017
    Resolves rhbz: 1213148
    Upstream-Liberty: https://review.openstack.org/#/c/174299/

---
+++ neutron_vpnaas/services/vpn/device_drivers/libreswan_ipsec.py
@@ -0,0 +1,50 @@
+# Copyright (c) 2015 Red Hat, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+from neutron_vpnaas.services.vpn.device_drivers import ipsec
+
+
+class LibreSwanProcess(ipsec.OpenSwanProcess):
+    """Libreswan Process manager class.
+
+    Libreswan needs nssdb initialised before running pluto daemon.
+    """
+    def __init__(self, conf, process_id, vpnservice, namespace):
+        super(LibreSwanProcess, self).__init__(conf, process_id,
+                                              vpnservice, namespace)
+
+    def ensure_configs(self):
+        """Generate config files which are needed for Libreswan.
+
+        Initialise the nssdb, otherwise pluto daemon will fail to run.
+        """
+        super(LibreSwanProcess, self).ensure_configs()
+        # Load the ipsec kernel module if not loaded
+        self._execute([self.binary, '_stackmanager', 'start'])
+        # checknss creates nssdb only if it is missing
+        # It is added in Libreswan version v3.10
+        # For prior versions use initnss
+        try:
+            self._execute([self.binary, 'checknss', self.etc_dir])
+        except RuntimeError:
+            self._execute([self.binary, 'initnss', self.etc_dir])
+
+
+class LibreSwanDriver(ipsec.IPsecDriver):
+    def create_process(self, process_id, vpnservice, namespace):
+        return LibreSwanProcess(
+            self.conf,
+            process_id,
+            vpnservice,
+            namespace)
