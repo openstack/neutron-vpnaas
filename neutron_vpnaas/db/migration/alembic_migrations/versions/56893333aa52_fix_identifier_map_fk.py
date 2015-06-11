c6692d2ce87027c3c3019223d5f14c1b792a1244
Vladislav Belogrudov <vladislav.belogrudov@oracle.com>
2015-06-11 13:28:12 +0300

    Resize cisco_csr_identifier_map.ipsec_site_conn_id
    Some database engines require exact match of sizes of foreign keys
    and referenced fields. Foreign key ipsec_site_conn_id of table
    cisco_csr_identifier_map is varchar(64) but it references field
    id varchar(36) of table ipsec_site_connections. This gives error
    while running migration scripts in such databases.
    
    Change-Id: I384a9bbaba05ef94174b666bdcfb276eedc74134
    Closes-Bug: #1463806

---
+++ neutron_vpnaas/db/migration/alembic_migrations/versions/56893333aa52_fix_identifier_map_fk.py
@@ -0,0 +1,48 @@
+# Copyright(c) 2015, Oracle and/or its affiliates.  All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""fix identifier map fk
+
+Revision ID: 56893333aa52
+Revises: kilo
+Create Date: 2015-06-11 12:09:01.263253
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '56893333aa52'
+down_revision = 'kilo'
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.sql import column
+from sqlalchemy.sql import expression as expr
+from sqlalchemy.sql import func
+from sqlalchemy.sql import table
+
+
+def upgrade():
+    # re-size existing data if necessary
+    identifier_map = table('cisco_csr_identifier_map',
+                           column('ipsec_site_conn_id', sa.String(36)))
+    ipsec_site_conn_id = identifier_map.columns['ipsec_site_conn_id']
+    op.execute(identifier_map.update(values={
+        ipsec_site_conn_id: expr.case([(func.length(ipsec_site_conn_id) > 36,
+                                      func.substr(ipsec_site_conn_id, 1, 36))],
+                                      else_=ipsec_site_conn_id)}))
+
+    op.alter_column(table_name='cisco_csr_identifier_map',
+                    column_name='ipsec_site_conn_id',
+                    type_=sa.String(36),
+                    existing_nullable=True)
