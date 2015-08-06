3de44041accdd7950ae615e58f57a8f2889ca3d4
Ann Kamyshnikova <akamyshnikova@mirantis.com>
2015-08-06 11:07:42 +0300

    Implement ModelMigrationSyncTest
    Test checks that the Python models for database tables are
    synchronized with the alembic migrations that create
    the database schema.
    
    This test should be implemented in all driver/plugin
    repositories that were split out from Neutron.
    
    Depends-On: I9a06de64ce35675af28adf819de6f22dc832390d
    
    Change-Id: I1d93d40ea235c228b7a27545471d88cb2a459c0b

---
+++ neutron_vpnaas/db/models/head.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2014 OpenStack Foundation.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""
+The module provides all database models at current HEAD.
+
+Its purpose is to create comparable metadata with current database schema.
+Based on this comparison database can be healed with healing migration.
+
+"""
+
+from neutron.db.migration.models import head
+
+from neutron_vpnaas.db.vpn import vpn_db  # noqa
+from neutron_vpnaas.services.vpn.service_drivers import cisco_csr_db  # noqa
+
+
+def get_metadata():
+    return head.model_base.BASEV2.metadata
