5996aab16e776dd25c2d3fa3a48d7806f9c0f86f
Brent Eagles <beagles@redhat.com>
2015-08-13 13:35:36 -0230

    Manage cleanup of .ctl/.pid files for LibreSwan
    LibreSwan checks for the presence of pid/ctl files when starting up and
    will error out if they already exist. However, LibreSwan's usage of the
    capabilities library removes the access required to cleanup .ctl and
    .pid files on shutdown if any of the directories in the path are missing
    explicit permissions for root. This is not considered a bug by the
    LibreSwan maintainers, so the LibreSwan driver must work around it by
    checking if it is okay to remove the files and removing them on
    startup. It must also wait for shutdown to complete before restarting
    the daemon on LibreSwanProcess.restart().
    
    Introduces new configuration for retrying check for process shutdown.
    
    Conflicts:
    	neutron_vpnaas/tests/unit/services/vpn/device_drivers/test_ipsec.py
    
    Closes-Bug: #1331502
    (cherry picked from commit 00b633d284f0f21aa380fa47a270c612ebef0795)
    Change-Id: I5c215d70c348524979b740f882029f74e400e6d7

--- neutron_vpnaas/services/vpn/device_drivers/libreswan_ipsec.py
+++ neutron_vpnaas/services/vpn/device_drivers/libreswan_ipsec.py
@@ -12,7 +12,36 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+import os
+import os.path
+
+import eventlet
+
+from neutron.i18n import _LE
+from neutron.i18n import _LW
+from oslo_config import cfg
+from oslo_log import log as logging
+
 from neutron_vpnaas.services.vpn.device_drivers import ipsec
+
+LOG = logging.getLogger(__name__)
+
+libreswan_opts = [
+    cfg.IntOpt('shutdown_check_timeout',
+               default=1,
+               help=_('Initial interval in seconds for checking if pluto '
+                      'daemon is shutdown')),
+    cfg.IntOpt('shutdown_check_retries',
+               default=5,
+               help=_('The maximum number of retries for checking for '
+                      'pluto daemon shutdown')),
+    cfg.FloatOpt('shutdown_check_back_off',
+                 default=1.5,
+                 help=_('A factor to increase the retry interval for '
+                        'each retry'))
+]
+
+cfg.CONF.register_opts(libreswan_opts, 'libreswan')
 
 
 class LibreSwanProcess(ipsec.OpenSwanProcess):
@@ -23,6 +52,7 @@ class LibreSwanProcess(ipsec.OpenSwanProcess):
     def __init__(self, conf, process_id, vpnservice, namespace):
         super(LibreSwanProcess, self).__init__(conf, process_id,
                                               vpnservice, namespace)
+        self.pid_file = '%s.pid' % self.pid_path
 
     def ensure_configs(self):
         """Generate config files which are needed for Libreswan.
@@ -39,6 +69,96 @@ class LibreSwanProcess(ipsec.OpenSwanProcess):
             self._execute([self.binary, 'checknss', self.etc_dir])
         except RuntimeError:
             self._execute([self.binary, 'initnss', self.etc_dir])
+
+    def _process_running(self):
+        """Checks if process is still running."""
+
+        # If no PID file, we assume the process is not running.
+        if not os.path.exists(self.pid_file):
+            return False
+
+        try:
+            # We take an ask-forgiveness-not-permission approach and rely
+            # on throwing to tell us something. If the pid file exists,
+            # delve into the process information and check if it matches
+            # our expected command line.
+            with open(self.pid_file, 'r') as f:
+                pid = f.readline().strip()
+                with open('/proc/%s/cmdline' % pid) as cmd_line_file:
+                    cmd_line = cmd_line_file.readline()
+                    if self.pid_path in cmd_line and 'pluto' in cmd_line:
+                        # Okay the process is probably a libreswan process
+                        # and it contains the pid_path in the command
+                        # line... could be a race. Log to error and return
+                        # that it is *NOT* okay to clean up files. We are
+                        # logging to error instead of debug because it
+                        # indicates something bad has happened and this is
+                        # valuable information for figuring it out.
+                        LOG.error(_LE('Process %(pid)s exists with command '
+                                  'line %(cmd_line)s.') %
+                                  {'pid': pid, 'cmd_line': cmd_line})
+                        return True
+
+        except IOError as e:
+            LOG.error(_LE('Unable to check control files on startup for '
+                          'router %(router)s: %(msg)s'),
+                      {'router': self.id, 'msg': e})
+        return False
+
+    def _cleanup_control_files(self):
+        try:
+            ctl_file = '%s.ctl' % self.pid_path
+            LOG.debug('Removing %(pidfile)s and %(ctlfile)s',
+                      {'pidfile': self.pid_file,
+                       'ctlfile': '%s.ctl' % ctl_file})
+
+            if os.path.exists(self.pid_file):
+                os.remove(self.pid_file)
+
+            if os.path.exists(ctl_file):
+                os.remove(ctl_file)
+
+        except OSError as e:
+            LOG.error(_LE('Unable to remove libreswan control '
+                          'files for router %(router)s. %(msg)s'),
+                      {'router': self.id, 'msg': e})
+
+    def start(self):
+        # NOTE: The restart operation calls the parent's start() instead of
+        # this one to avoid having to special case the startup file check.
+        # If anything is added to this method that needs to run whenever
+        # a restart occurs, it should be either added to the restart()
+        # override or things refactored to special-case start() when
+        # called from restart().
+
+        # LibreSwan's use of the capablities library may prevent the ctl
+        # and pid files from being cleaned up, so we check to see if the
+        # process is running and if not, attempt a cleanup. In either case
+        # we fall through to allow the LibreSwan process to start or fail
+        # in the usual way.
+        if not self._process_running():
+            self._cleanup_control_files()
+
+        super(LibreSwanProcess, self).start()
+
+    def restart(self):
+        # stop() followed immediately by a start() runs the risk that the
+        # current pluto daemon has not had a chance to shutdown. We check
+        # the current process information to see if the daemon is still
+        # running and if so, wait a short interval and retry.
+        self.stop()
+        wait_interval = cfg.CONF.libreswan.shutdown_check_timeout
+        for i in range(cfg.CONF.libreswan.shutdown_check_retries):
+            if not self._process_running():
+                self._cleanup_control_files()
+                break
+            eventlet.sleep(wait_interval)
+            wait_interval *= cfg.CONF.libreswan.shutdown_check_back_off
+        else:
+            LOG.warning(_LW('Server appears to still be running, restart '
+                            'of router %s may fail'), self.id)
+
+        super(LibreSwanProcess, self).start()
 
 
 class LibreSwanDriver(ipsec.IPsecDriver):
