a1b12601a64a2359b2224fd4406c5db008484700
Ihar Hrachyshka <ihrachys@redhat.com>
2016-03-30 11:52:18 +0200

    Constraint requirements using mitaka upper-constraints.txt file
    While at it, use stable/mitaka branch for dependencies pulled by
    tools/tox_install.sh.
    
    Change-Id: I6a908470987ce05769c425c4623df404cef76a10

--- tools/tox_install.sh
+++ tools/tox_install.sh
@@ -18,7 +18,7 @@ set -x
 ZUUL_CLONER=/usr/zuul-env/bin/zuul-cloner
 neutron_installed=$(echo "import neutron" | python 2>/dev/null ; echo $?)
 NEUTRON_DIR=$HOME/neutron
-BRANCH_NAME=master
+BRANCH_NAME=stable/mitaka
 
 set -e
 
