c21d3acb8f55588b4e1c31d6b0295277de76d35d
Henry Gessau <HenryG@gessau.net>
2016-08-31 14:10:34 -0400

    Use model_base from neutron-lib
    Related Blueprint: neutron-lib
    
    Depends-On: If5b2b4cc0346515ddef3da1255ab49327c8e5732
    
    Change-Id: I51b2250a893bea88abbc2e36017056936d78b943

--- neutron_vpnaas/db/vpn/vpn_models.py
+++ neutron_vpnaas/db/vpn/vpn_models.py
@@ -13,12 +13,12 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from neutron_lib.db import model_base
 import sqlalchemy as sa
 from sqlalchemy import orm
 
 from neutron.api.v2 import attributes as attr
 from neutron.db import l3_db
-from neutron.db import model_base
 from neutron.db import models_v2
 
 from neutron_vpnaas.services.vpn.common import constants
