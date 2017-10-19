5eb58a2a4bd2da237aa70506c84d331974955741
Luong Anh Tuan <tuanla@vn.fujitsu.com>
2017-10-19 15:29:33 +0700

    Redundant alias in import statement
    Change-Id: I263758b6710eaafb16a1dec1f24e00546dc3e803

--- rally-jobs/plugins/test_vpn_connectivity.py
+++ rally-jobs/plugins/test_vpn_connectivity.py
@@ -14,7 +14,7 @@
 
 from rally.common import logging
 from rally.task import scenario
-from rally.task import types as types
+from rally.task import types
 
 import vpn_base
 LOG = logging.getLogger(__name__)
